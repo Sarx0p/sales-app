feea409fe3e256597bb6bcd97305db0afdb922f9
Hembert <ap25002@itcha.edu.sv>
2026-04-08 10:31:20 -0600

    Cliente funcional, ya se puede agregar cliente

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+route::resources([
+    'clientes' => ClienteController::class,
+]);
